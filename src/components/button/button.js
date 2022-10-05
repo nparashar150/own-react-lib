f4f86dcfc515d9dd694b2117b1dce2fa209e41a7
nparashar150 <nparashar150@gmail.com>
2022-10-06 01:40:34 +0530

    * Modify createElement as per jsx * Allow unused OwnReact imports

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,5 +1,7 @@
-const Button = () => {
-  return <button>Click Me</button>;
+import OwnReact from "../../../lib/index";
+
+const Button = (children) => {
+  return <button>{children}</button>;
 };
 
 export default Button;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import OwnReact from "../lib/index";
-// import Button from "./components/button/button";
+import Button from "./components/button/button";
 
 const App = () => {
   return (
     <div id="root2">
+      <Button>Click me</Button>
       <h1>My First Own React App</h1>
-      {/* <Button /> */}
       <p>
         <div className="testing">
           <h1>My First Own React App with custom Babel and Webpack config</h1>
